f0109cdf57a41332ea227459a64c8628ef069334
DaryaBoychenko <daryam963@yandex.ru>
2024-11-21 15:34:57 +0300

    first commit

---
+++ laravel-react/resources/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/Example'
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<Example />)
+
+
---
+++ laravel-react/resources/js/components/Example.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Example = () => {
+    retiurn (
+        <div>
+            <h1>Hellow</h1>
+        </div>
+    )
+}
+
+export default Example;
